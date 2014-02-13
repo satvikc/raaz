ce36ad82a4c744e7158f4df71faf607ec157fe7b
satvikc <mystic.satvik@gmail.com>
2014-02-13 19:51:43 +0530

    Initial receivePayload function added

--- raaz-ssh/Raaz/Network/SSH/Transport.hs
+++ raaz-ssh/Raaz/Network/SSH/Transport.hs
@@ -9,13 +9,29 @@ transport layer of ssh.
 module Raaz.Network.SSH.Transport
        ( idString
        , idString'
+       , receivePayload
        ) where
 
-import qualified Data.ByteString.Char8 as C8
-import           Data.Version(showVersion)
+import           Control.Applicative
+import           Control.Exception
+import           Control.Monad                   (when)
+import qualified Data.ByteString.Char8           as C8
+import           Data.ByteString                 (ByteString)
+import           Data.Version                    (showVersion)
 
-import           Paths_raaz_ssh(version)
+import           Raaz.Memory
+import           Raaz.Parse                      (runParser)
+import           Raaz.Primitives
+import           Raaz.Primitives.Cipher
+import           Raaz.Types
+import           Raaz.Util.ByteString
+import           Raaz.Util.Ptr
 
+import           Paths_raaz_ssh                  (version)
+import           Raaz.Network.SSH.Backend
+import           Raaz.Network.SSH.Context        (Context(..))
+import           Raaz.Network.SSH.Exception
+import           Raaz.Network.SSH.Message.Parser
 
 -- | The prefix of the identification string.
 idPrefix = C8.concat [ "SSH-2.0-"
@@ -30,3 +46,45 @@ idString = idPrefix `C8.append` "\r\n"
 -- | The identification string with an extra comment field.
 idString' :: C8.ByteString -> C8.ByteString
 idString' comment = C8.concat [ idPrefix, " ", comment, "\r\n"]
+
+
+-- | Performs the MAC verification and decryption and returns the
+-- payload as a `ByteString`.
+receivePayload :: Context -> IO ByteString
+receivePayload (Context buff sock _ decr _) = do
+  (packLen,padLen) <- withBuffer buff $ decrypt decr
+  ok <- withBuffer buff verify
+  if ok
+    then withBuffer buff $ createFrom (packLen - padLen - 1) . (`movePtr` headerSize)
+    else throwIO $ TransportE MACFailure
+  where
+    -- Verify MAC
+    verify = undefined
+    -- Decrypt and return (packet length, padding length)
+    decrypt :: Gadget g => Maybe g -> CryptoPtr -> IO (BYTES Int,BYTES Int)
+    decrypt Nothing buffptr = do
+      receive sock headerSize buffptr
+      (packLen,padLen) <- runParser buffptr $ (,) <$> (fromIntegral <$> parseWord32BE)
+                                                  <*> (fromIntegral <$> parseByte)
+      receive sock (packLen - 1 + macLen) buffptr
+      return (packLen,padLen)
+    decrypt (Just g) buffptr = do
+      receive sock nBytes buffptr
+      apply g nBlocks buffptr
+      (packLen,padLen) <- runParser buffptr $ (,) <$> (fromIntegral <$> parseWord32BE)
+                                                  <*> (fromIntegral <$> parseByte)
+      let movedPtr = buffptr `movePtr` nBytes
+      receive sock (packLen - 1 + macLen) buffptr
+      apply g (cryptoCoerce $ packLen + nBytes - 4) movedPtr
+      return (packLen,padLen)
+      where
+        singleBlock = 1
+        (q,r) = headerSize `quotRem` cryptoCoerce singleBlock
+        -- Ceil header size to cipher block size
+        nBlocks = if r == 0 then fromIntegral q else fromIntegral q + singleBlock
+        nBytes = fromIntegral nBlocks :: BYTES Int
+    headerSize = 5 :: BYTES Int -- Cipher must decrypt atleast 5 bytes to calculate packet and padding length
+    macLen = undefined
+
+sendPayload :: Context -> ByteString -> IO ()
+sendPayload (Context buff sock encr _ _) = undefined
