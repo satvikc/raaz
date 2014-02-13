ebfc02f1001ba033cd6370e21abe01dce32408d3
satvikc <mystic.satvik@gmail.com>
2014-02-13 16:53:47 +0530

    Exported functions to refer to crypto primitives

--- raaz-ssh/Raaz/Network/SSH/Naming.hs
+++ raaz-ssh/Raaz/Network/SSH/Naming.hs
@@ -8,11 +8,18 @@ Standard naming for primitives required while algorithm negotiation.
 {-# LANGUAGE FlexibleInstances  #-}
 module Raaz.Network.SSH.Naming
        ( MethodName(..)
+         -- | Hash Primitives (which are just `undefined` with the correct type)
+       , sha1, sha224, sha256, sha384, sha512
+         -- | AES Primitives (which are just `undefined` with the correct type)
+       , aes256ctrE, aes192ctrE, aes128ctrE
+       , aes256ctrD, aes192ctrD, aes128ctrD
+       , aes256cbcE, aes192cbcE, aes128cbcE
+       , aes256cbcD, aes192cbcD, aes128cbcD
        ) where
 
 
 import           Raaz.Primitives
-import           Raaz.Hash
+import           Raaz.Hash (SHA1,SHA224,SHA256,SHA384,SHA512)
 import           Raaz.Primitives.Cipher
 import           Raaz.Cipher.AES.Type
 import           Raaz.Cipher.AES.CBC    ()
@@ -21,7 +28,7 @@ import           Raaz.Cipher.AES.CTR    ()
 import qualified Data.ByteString.Char8  as C8
 
 -- | Standard Naming required while algorithm negotiations.
-class Primitive p => MethodName p where
+class MethodName p where
   methodName :: p -> C8.ByteString
 
 -- | `MethodName` instances for hash algorithms.
@@ -87,3 +94,54 @@ instance MethodName (AES256 CTR Encryption) where
 
 instance MethodName (AES256 CTR Decryption) where
   methodName _ = "aes256-ctr"
+
+sha1 :: SHA1
+sha1 = undefined
+
+sha224 :: SHA224
+sha224 = undefined
+
+sha256 :: SHA256
+sha256 = undefined
+
+sha384 :: SHA384
+sha384 = undefined
+
+sha512 :: SHA512
+sha512 = undefined
+
+aes256ctrE :: AES256 CTR Encryption
+aes256ctrE = undefined
+
+aes256cbcE :: AES256 CBC Encryption
+aes256cbcE = undefined
+
+aes192ctrE :: AES192 CTR Encryption
+aes192ctrE = undefined
+
+aes192cbcE :: AES192 CBC Encryption
+aes192cbcE = undefined
+
+aes128ctrE :: AES128 CTR Encryption
+aes128ctrE = undefined
+
+aes128cbcE :: AES128 CBC Encryption
+aes128cbcE = undefined
+
+aes256ctrD :: AES256 CTR Decryption
+aes256ctrD = undefined
+
+aes256cbcD :: AES256 CBC Decryption
+aes256cbcD = undefined
+
+aes192ctrD :: AES192 CTR Decryption
+aes192ctrD = undefined
+
+aes192cbcD :: AES192 CBC Decryption
+aes192cbcD = undefined
+
+aes128ctrD :: AES128 CTR Decryption
+aes128ctrD = undefined
+
+aes128cbcD :: AES128 CBC Decryption
+aes128cbcD = undefined
